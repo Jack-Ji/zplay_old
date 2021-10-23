a9da36228323c2c8f43d9f4a5cc66e1c87aa5a20
jackji <jichengde@qq.com>
2021-10-23 19:52:30 +0800

    improve example

--- examples/simple_triangle.zig
+++ examples/simple_triangle.zig
@@ -5,20 +5,25 @@ const gl = zp.gl;
 const vertex_shader =
     \\#version 330 core
     \\layout (location = 0) in vec3 a_pos;
+    \\layout (location = 1) in vec3 a_color;
+    \\
+    \\out vec3 vertex_color;
     \\
     \\void main()
     \\{
     \\    gl_Position = vec4(a_pos, 1.0);
+    \\    vertex_color = a_color;
     \\}
 ;
 
 const fragment_shader =
     \\#version 330 core
+    \\in vec3 vertex_color;
     \\out vec4 frag_color;
     \\
     \\void main()
     \\{
-    \\    frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \\    frag_color = vec4(vertex_color, 1.0);
     \\}
 ;
 
@@ -26,9 +31,10 @@ var shader_program: gl.ShaderProgram = undefined;
 var vertex_array: gl.VertexArray = undefined;
 
 const vertices = [_]f32{
-    -0.5, -0.5, 0.0,
-    0.5,  -0.5, 0.0,
-    0.0,  0.5,  0.0,
+    // positions     // colors
+    -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+    0.5,  -0.5, 0.0, 0.0, 1.0, 0.0,
+    0.0,  0.5,  0.0, 0.0, 0.0, 1.0,
 };
 
 fn init(ctx: *zp.Context) anyerror!void {
@@ -42,7 +48,8 @@ fn init(ctx: *zp.Context) anyerror!void {
     vertex_array.use();
     defer vertex_array.disuse();
     vertex_array.bufferData(0, f32, &vertices, .array_buffer, .static_draw);
-    vertex_array.setAttribute(0, 3, f32, false, 3 * @sizeOf(f32), 0);
+    vertex_array.setAttribute(0, 3, f32, false, 6 * @sizeOf(f32), 0);
+    vertex_array.setAttribute(1, 3, f32, false, 6 * @sizeOf(f32), 3 * @sizeOf(f32));
 
     std.log.info("game init", .{});
 }
