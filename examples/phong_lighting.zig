9aba43fdb8d0466d64c63729198dc50c84cd7ea9
jackji <jichengde@qq.com>
2021-11-10 12:48:24 +0800

    improve Material3D

--- examples/phong_lighting.zig
+++ examples/phong_lighting.zig
@@ -7,9 +7,11 @@ const vertex_shader =
     \\#version 330 core
     \\layout (location = 0) in vec3 a_pos;
     \\layout (location = 1) in vec3 a_normal;
+    \\layout (location = 2) in vec2 a_tex;
     \\
     \\out vec3 v_pos;
     \\out vec3 v_normal;
+    \\out vec2 v_tex;
     \\
     \\uniform mat4 u_model;
     \\uniform mat4 u_normal;
@@ -21,6 +23,7 @@ const vertex_shader =
     \\    gl_Position = u_project * u_view * u_model * vec4(a_pos, 1.0);
     \\    v_pos = vec3(u_model * vec4(a_pos, 1.0));
     \\    v_normal = mat3(u_normal) * a_normal;
+    \\    v_tex = a_tex;
     \\}
 ;
 
@@ -30,6 +33,7 @@ const fragment_shader =
     \\
     \\in vec3 v_pos;
     \\in vec3 v_normal;
+    \\in vec2 v_tex;
     \\
     \\uniform vec3 u_view_pos;
     \\
@@ -42,9 +46,8 @@ const fragment_shader =
     \\uniform Light u_light;
     \\
     \\struct Material {
-    \\    vec3 ambient;
-    \\    vec3 diffuse;
-    \\    vec3 specular;
+    \\    sampler2D diffuse;
+    \\    sampler2D specular;
     \\    float shiness;
     \\};
     \\uniform Material u_material;
@@ -85,13 +88,20 @@ const fragment_shader =
     \\
     \\void main()
     \\{
-    \\    vec3 ambient_color = ambientColor(u_light.ambient, u_material.ambient);
-    \\    vec3 diffuse_color = diffuseColor(u_light.diffuse, u_light.position,
-    \\                                      v_pos, v_normal, u_material.diffuse);
-    \\    vec3 specular_color = specularColor(u_light.specular, u_light.position, u_view_pos,
-    \\                                        v_pos, v_normal, u_material.specular,
+    \\    vec3 ambient_color = ambientColor(u_light.ambient,
+    \\                                      vec3(texture(u_material.diffuse, v_tex)));
+    \\    vec3 diffuse_color = diffuseColor(u_light.diffuse,
+    \\                                      u_light.position,
+    \\                                      v_pos,
+    \\                                      v_normal,
+    \\                                      vec3(texture(u_material.diffuse, v_tex)));
+    \\    vec3 specular_color = specularColor(u_light.specular,
+    \\                                        u_light.position,
+    \\                                        u_view_pos,
+    \\                                        v_pos,
+    \\                                        v_normal,
+    \\                                        vec3(texture(u_material.specular, v_tex)),
     \\                                        u_material.shiness);
-    \\
     \\    vec3 result = ambient_color + diffuse_color + specular_color;
     \\    frag_color = vec4(result, 1.0);
     \\}
@@ -123,69 +133,51 @@ var light = zp.util.Light3D.init(
     alg.Vec3.new(0.5, 0.5, 0.5),
     null,
 );
-var materials = [_]zp.util.Material3D{
-    zp.util.Material3D.init(
-        alg.Vec3.new(1, 0.5, 0.31),
-        alg.Vec3.new(1, 0.5, 0.31),
-        alg.Vec3.new(0.5, 0.5, 0.5),
-        32,
-    ),
-    zp.util.Material3D.emerald,
-    zp.util.Material3D.jade,
-    zp.util.Material3D.obsidian,
-    zp.util.Material3D.pearl,
-    zp.util.Material3D.ruby,
-    zp.util.Material3D.turquoise,
-    zp.util.Material3D.brass,
-    zp.util.Material3D.bronze,
-    zp.util.Material3D.chrome,
-    zp.util.Material3D.copper,
-    zp.util.Material3D.gold,
-    zp.util.Material3D.silver,
-};
+var material: zp.util.Material3D = undefined;
 
+// position, normal, texture coord
 const vertices = [_]f32{
-    -0.5, -0.5, -0.5, 0.0,  0.0,  -1.0,
-    0.5,  -0.5, -0.5, 0.0,  0.0,  -1.0,
-    0.5,  0.5,  -0.5, 0.0,  0.0,  -1.0,
-    0.5,  0.5,  -0.5, 0.0,  0.0,  -1.0,
-    -0.5, 0.5,  -0.5, 0.0,  0.0,  -1.0,
-    -0.5, -0.5, -0.5, 0.0,  0.0,  -1.0,
+    -0.5, -0.5, -0.5, 0.0,  0.0,  -1.0, 0.0, 0.0,
+    0.5,  -0.5, -0.5, 0.0,  0.0,  -1.0, 1.0, 0.0,
+    0.5,  0.5,  -0.5, 0.0,  0.0,  -1.0, 1.0, 1.0,
+    0.5,  0.5,  -0.5, 0.0,  0.0,  -1.0, 1.0, 1.0,
+    -0.5, 0.5,  -0.5, 0.0,  0.0,  -1.0, 0.0, 1.0,
+    -0.5, -0.5, -0.5, 0.0,  0.0,  -1.0, 0.0, 0.0,
 
-    -0.5, -0.5, 0.5,  0.0,  0.0,  1.0,
-    0.5,  -0.5, 0.5,  0.0,  0.0,  1.0,
-    0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
-    0.5,  0.5,  0.5,  0.0,  0.0,  1.0,
-    -0.5, 0.5,  0.5,  0.0,  0.0,  1.0,
-    -0.5, -0.5, 0.5,  0.0,  0.0,  1.0,
+    -0.5, -0.5, 0.5,  0.0,  0.0,  1.0,  0.0, 0.0,
+    0.5,  -0.5, 0.5,  0.0,  0.0,  1.0,  1.0, 0.0,
+    0.5,  0.5,  0.5,  0.0,  0.0,  1.0,  1.0, 1.0,
+    0.5,  0.5,  0.5,  0.0,  0.0,  1.0,  1.0, 1.0,
+    -0.5, 0.5,  0.5,  0.0,  0.0,  1.0,  0.0, 1.0,
+    -0.5, -0.5, 0.5,  0.0,  0.0,  1.0,  0.0, 0.0,
 
-    -0.5, 0.5,  0.5,  -1.0, 0.0,  0.0,
-    -0.5, 0.5,  -0.5, -1.0, 0.0,  0.0,
-    -0.5, -0.5, -0.5, -1.0, 0.0,  0.0,
-    -0.5, -0.5, -0.5, -1.0, 0.0,  0.0,
-    -0.5, -0.5, 0.5,  -1.0, 0.0,  0.0,
-    -0.5, 0.5,  0.5,  -1.0, 0.0,  0.0,
+    -0.5, 0.5,  0.5,  -1.0, 0.0,  0.0,  1.0, 0.0,
+    -0.5, 0.5,  -0.5, -1.0, 0.0,  0.0,  1.0, 1.0,
+    -0.5, -0.5, -0.5, -1.0, 0.0,  0.0,  0.0, 1.0,
+    -0.5, -0.5, -0.5, -1.0, 0.0,  0.0,  0.0, 1.0,
+    -0.5, -0.5, 0.5,  -1.0, 0.0,  0.0,  0.0, 0.0,
+    -0.5, 0.5,  0.5,  -1.0, 0.0,  0.0,  1.0, 0.0,
 
-    0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
-    0.5,  0.5,  -0.5, 1.0,  0.0,  0.0,
-    0.5,  -0.5, -0.5, 1.0,  0.0,  0.0,
-    0.5,  -0.5, -0.5, 1.0,  0.0,  0.0,
-    0.5,  -0.5, 0.5,  1.0,  0.0,  0.0,
-    0.5,  0.5,  0.5,  1.0,  0.0,  0.0,
+    0.5,  0.5,  0.5,  1.0,  0.0,  0.0,  1.0, 0.0,
+    0.5,  0.5,  -0.5, 1.0,  0.0,  0.0,  1.0, 1.0,
+    0.5,  -0.5, -0.5, 1.0,  0.0,  0.0,  0.0, 1.0,
+    0.5,  -0.5, -0.5, 1.0,  0.0,  0.0,  0.0, 1.0,
+    0.5,  -0.5, 0.5,  1.0,  0.0,  0.0,  0.0, 0.0,
+    0.5,  0.5,  0.5,  1.0,  0.0,  0.0,  1.0, 0.0,
 
-    -0.5, -0.5, -0.5, 0.0,  -1.0, 0.0,
-    0.5,  -0.5, -0.5, 0.0,  -1.0, 0.0,
-    0.5,  -0.5, 0.5,  0.0,  -1.0, 0.0,
-    0.5,  -0.5, 0.5,  0.0,  -1.0, 0.0,
-    -0.5, -0.5, 0.5,  0.0,  -1.0, 0.0,
-    -0.5, -0.5, -0.5, 0.0,  -1.0, 0.0,
+    -0.5, -0.5, -0.5, 0.0,  -1.0, 0.0,  0.0, 1.0,
+    0.5,  -0.5, -0.5, 0.0,  -1.0, 0.0,  1.0, 1.0,
+    0.5,  -0.5, 0.5,  0.0,  -1.0, 0.0,  1.0, 0.0,
+    0.5,  -0.5, 0.5,  0.0,  -1.0, 0.0,  1.0, 0.0,
+    -0.5, -0.5, 0.5,  0.0,  -1.0, 0.0,  0.0, 0.0,
+    -0.5, -0.5, -0.5, 0.0,  -1.0, 0.0,  0.0, 1.0,
 
-    -0.5, 0.5,  -0.5, 0.0,  1.0,  0.0,
-    0.5,  0.5,  -0.5, 0.0,  1.0,  0.0,
-    0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
-    0.5,  0.5,  0.5,  0.0,  1.0,  0.0,
-    -0.5, 0.5,  0.5,  0.0,  1.0,  0.0,
-    -0.5, 0.5,  -0.5, 0.0,  1.0,  0.0,
+    -0.5, 0.5,  -0.5, 0.0,  1.0,  0.0,  0.0, 1.0,
+    0.5,  0.5,  -0.5, 0.0,  1.0,  0.0,  1.0, 1.0,
+    0.5,  0.5,  0.5,  0.0,  1.0,  0.0,  1.0, 0.0,
+    0.5,  0.5,  0.5,  0.0,  1.0,  0.0,  1.0, 0.0,
+    -0.5, 0.5,  0.5,  0.0,  1.0,  0.0,  0.0, 0.0,
+    -0.5, 0.5,  -0.5, 0.0,  1.0,  0.0,  0.0, 1.0,
 };
 
 fn init(ctx: *zp.Context) anyerror!void {
@@ -200,8 +192,18 @@ fn init(ctx: *zp.Context) anyerror!void {
     cube_va.use();
     defer cube_va.disuse();
     cube_va.bufferData(0, f32, &vertices, .array_buffer, .static_draw);
-    cube_va.setAttribute(0, 3, f32, false, 6 * @sizeOf(f32), 0);
-    cube_va.setAttribute(1, 3, f32, false, 6 * @sizeOf(f32), 3 * @sizeOf(f32));
+    cube_va.setAttribute(0, 3, f32, false, 8 * @sizeOf(f32), 0);
+    cube_va.setAttribute(1, 3, f32, false, 8 * @sizeOf(f32), 3 * @sizeOf(f32));
+    cube_va.setAttribute(2, 2, f32, false, 8 * @sizeOf(f32), 6 * @sizeOf(f32));
+
+    // material init
+    var diffuse_texture = try zp.util.Texture2D.init("assets/container2.png", null, false);
+    var specular_texture = try zp.util.Texture2D.init("assets/container2_specular.png", .texture_unit_1, false);
+    material = zp.util.Material3D.init(
+        diffuse_texture,
+        specular_texture,
+        32,
+    );
 
     // enable depth test
     gl.util.toggleCapability(.depth_test, true);
@@ -210,10 +212,6 @@ fn init(ctx: *zp.Context) anyerror!void {
 }
 
 fn loop(ctx: *zp.Context) void {
-    const S = struct {
-        var current_material: usize = 0;
-    };
-
     // camera movement
     const distance = ctx.delta_tick * camera.move_speed;
     if (ctx.isKeyPressed(.w)) {
@@ -244,20 +242,6 @@ fn loop(ctx: *zp.Context) void {
                     switch (key.scan_code) {
                         .escape => ctx.kill(),
                         .f1 => ctx.toggleFullscreeen(null),
-                        .up => {
-                            if (S.current_material == 0) {
-                                S.current_material = materials.len - 1;
-                            } else {
-                                S.current_material -= 1;
-                            }
-                        },
-                        .down => {
-                            if (S.current_material == materials.len - 1) {
-                                S.current_material = 0;
-                            } else {
-                                S.current_material += 1;
-                            }
-                        },
                         else => {},
                     }
                 }
@@ -312,7 +296,7 @@ fn loop(ctx: *zp.Context) void {
     normal_shader_program.setUniformByName("u_project", projection);
     normal_shader_program.setUniformByName("u_view_pos", camera.position);
     light.apply(&normal_shader_program, "u_light");
-    materials[S.current_material].apply(&normal_shader_program, "u_material");
+    material.apply(&normal_shader_program, "u_material");
     gl.util.drawBuffer(.triangles, 0, 36);
 
     // draw light
