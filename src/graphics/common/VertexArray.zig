3b5e1e705f6684130b9f4a8323f3471487dd1421
jackji <jichengde@qq.com>
2022-01-11 11:20:28 +0800

    enhance VertexArray

--- src/graphics/common/VertexArray.zig
+++ src/graphics/common/VertexArray.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 const zp = @import("../../zplay.zig");
 const gl = zp.deps.gl;
 const Self = @This();
@@ -32,6 +33,13 @@ const BufferUsage = enum(c_uint) {
     dynamic_copy = gl.GL_DYNAMIC_COPY,
 };
 
+/// buffer data's access level
+const BufferAccess = enum(c_uint) {
+    read = gl.GL_READ_ONLY,
+    write = gl.GL_WRITE_ONLY,
+    read_write = gl.GL_READ_WRITE,
+};
+
 /// id of vertex array
 id: gl.GLuint = undefined,
 
@@ -41,13 +49,7 @@ vbo_num: u32 = undefined,
 
 /// init vertex array
 pub fn init(vbo_num: u32) Self {
-    if (vbo_num == 0) {
-        std.debug.panic("invalid parameter", .{});
-    }
-    if (vbo_num > MAX_VBO_NUM) {
-        std.debug.panic("do you really need that many vbo?", .{});
-    }
-
+    assert(vbo_num > 0 and vbo_num <= MAX_VBO_NUM);
     var va: Self = undefined;
     gl.genVertexArrays(1, &va.id);
     va.vbo_num = vbo_num;
@@ -63,7 +65,7 @@ pub fn deinit(self: Self) void {
     gl.util.checkError();
 }
 
-/// update buffer data
+/// allocate and initialize buffer data
 pub fn bufferData(
     self: Self,
     vbo_index: u32,
@@ -72,10 +74,7 @@ pub fn bufferData(
     target: BufferTarget,
     usage: BufferUsage,
 ) void {
-    if (vbo_index > self.vbo_num) {
-        std.debug.panic("invalid vbo index", .{});
-    }
-
+    assert(vbo_index < self.vbo_num);
     gl.bindBuffer(@enumToInt(target), self.vbos[vbo_index]);
     gl.bufferData(
         @enumToInt(target),
@@ -84,6 +83,87 @@ pub fn bufferData(
         @enumToInt(usage),
     );
     gl.util.checkError();
+}
+
+/// only allocate buffer data
+pub fn bufferDataAlloc(
+    self: Self,
+    vbo_index: u32,
+    size: u32,
+    target: BufferTarget,
+    usage: BufferUsage,
+) void {
+    assert(vbo_index < self.vbo_num);
+    gl.bindBuffer(@enumToInt(target), self.vbos[vbo_index]);
+    gl.bufferData(
+        @enumToInt(target),
+        @intCast(c_longlong, size),
+        null,
+        @enumToInt(usage),
+    );
+    gl.util.checkError();
+}
+
+/// update buffer data, user need to make sure enough memory had been allocated
+pub fn bufferSubData(
+    self: Self,
+    vbo_index: u32,
+    offset: u32,
+    comptime T: type,
+    data: []const T,
+    target: BufferTarget,
+) void {
+    assert(vbo_index < self.vbo_num);
+    gl.bindBuffer(@enumToInt(target), self.vbos[vbo_index]);
+    gl.bufferSubData(
+        @enumToInt(target),
+        @intCast(c_longlong, offset),
+        @intCast(c_longlong, data.len * @sizeOf(T)),
+        data.ptr,
+    );
+    gl.util.checkError();
+}
+
+/// copy buffer data from gpu
+pub fn getBufferData(
+    self: Self,
+    vbo_index: u32,
+    offset: u32,
+    data: []const u8,
+    target: BufferTarget,
+) void {
+    assert(vbo_index < self.vbo_num);
+    gl.bindBuffer(@enumToInt(target), self.vbos[vbo_index]);
+    gl.getBufferSubData(
+        @enumToInt(target),
+        @intCast(c_longlong, offset),
+        @intCast(c_longlong, data.len),
+        data.ptr,
+    );
+    gl.util.checkError();
+}
+
+/// get mapped memory pointer
+pub fn mapBuffer(
+    self: Self,
+    vbo_index: u32,
+    target: BufferTarget,
+    access: BufferAccess,
+) ?[*]u8 {
+    assert(vbo_index < self.vbo_num);
+    gl.bindBuffer(@enumToInt(target), self.vbos[vbo_index]);
+    var data = gl.mapBuffer(@enumToInt(target), @enumToInt(access));
+    if (data) |ptr| {
+        return @ptrCast([*]u8, ptr);
+    }
+    return null;
+}
+
+/// unmap specified buffer object
+/// returns true unless data store become corrupted, which means user needs to reinitialize data.
+pub fn unmapBuffer(self: Self, target: BufferTarget) bool {
+    _ = self;
+    return gl.util.boolType(gl.unmapBuffer(@enumToInt(target)));
 }
 
 // set vertex attribute (will enable attribute afterwards)
@@ -97,6 +177,7 @@ pub fn setAttribute(
     stride: u32,
     offset: u32,
 ) void {
+    assert(vbo_index < self.vbo_num);
     gl.bindBuffer(
         @enumToInt(BufferTarget.array_buffer),
         self.vbos[vbo_index],
