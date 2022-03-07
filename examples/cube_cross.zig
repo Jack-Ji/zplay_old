80db6b90613815e6bfea914d774dedaeb23c24dd
jackji <jichengde@gmail.com>
2022-03-07 18:51:42 +0800

    minor tweak of sample code

--- examples/cube_cross.zig
+++ examples/cube_cross.zig
@@ -181,6 +181,7 @@ fn beforeRenderingCube(ctx: *Context, custom: ?*anyopaque) void {
 
 fn loop(ctx: *zp.Context) void {
     while (ctx.pollEvent()) |e| {
+        _ = dig.processEvent(e);
         switch (e) {
             .keyboard_event => |key| {
                 if (key.trigger_type == .up) {
