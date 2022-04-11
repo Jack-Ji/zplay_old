c4a919b95059789abeb34f5da391ae0eeb6ec3d1
jackji <jichengde@gmail.com>
2022-04-12 01:34:07 +0800

    chipmunk integration work

--- src/physics/CPWorld.zig
+++ src/physics/CPWorld.zig
@@ -15,6 +15,276 @@ const Mat4 = alg.Mat4;
 const Quat = alg.Quat;
 const Self = @This();
 
-const Object = struct {
-    size: Vec3,
+pub const Error = error{
+    OutOfMemory,
 };
+
+pub const Filter = struct {
+    group: usize = 0,
+    categories: u32 = ~@as(u32, 0),
+    mask: u32 = ~@as(u32, 0),
+};
+
+pub const Object = struct {
+    /// object's physics body, null means using global static
+    body: ?*cp.Shape,
+
+    /// object's shape
+    shapes: []*cp.Shape,
+
+    /// filter info
+    filter: Filter,
+};
+
+/// memory allocator
+allocator: std.mem.Allocator,
+
+/// physics world object
+space: *cp.Space,
+
+/// objects in the world
+objects: std.ArrayList(Object),
+
+/// init chipmunk world
+pub const CollisionCallback = struct {
+    type_a: ?cp.CollisionType = null,
+    type_b: ?cp.CollisionType = null,
+    begin_func: cp.CollisionBeginFunc = null,
+    pre_solve_func: cp.CollisionPreSolveFunc = null,
+    post_solve_func: cp.CollisionPostSolveFunc = null,
+    separate_func: cp.CollisionSeparateFunc = null,
+    user_data: cp.DataPointer = null,
+};
+pub const InitOption = struct {
+    gravity: cp.Vect = .{ .x = 0, .y = 10 },
+    dumping: f32 = 0,
+    iteration: u32 = 10,
+    user_data: cp.DataPointer = null,
+    collision_callbacks: []CollisionCallback = &.{},
+    prealloc_objects_num: u32 = 100,
+};
+pub fn init(allocator: std.mem.Allocator, opt: InitOption) !Self {
+    var space = cp.spaceNew();
+    if (space == null) return error.OutOfMemory;
+
+    cp.spaceSetGravity(space, opt.gravity);
+    cp.spaceSetDamping(space, opt.dumping);
+    cp.spaceSetIterations(space, @intCast(c_int, opt.iteration));
+    cp.spaceSetUserData(space, opt.user_data);
+    for (opt.collision_callbacks) |cb| {
+        var handler: *cp.CollisionHandler = undefined;
+        if (cb.type_a != null and cb.type_b != null) {
+            handler = cp.spaceAddCollisionHandler(space, cb.type_a.?, cb.type_b.?);
+        } else if (cb.type_a != null) {
+            handler = cp.spaceAddWildcardHandler(space, cb.type_a.?);
+        } else {
+            handler = cp.spaceAddDefaultCollisionHandler(space);
+        }
+        handler.beginFunc = cb.begin_func;
+        handler.preSolveFunc = cb.pre_solve_func;
+        handler.postSolveFunc = cb.post_solve_func;
+        handler.separateFunc = cb.separate_func;
+        handler.userData = cb.user_data;
+    }
+
+    return Self{
+        .allocator = allocator,
+        .space = space.?,
+        .objects = try std.ArrayList(Object).initCapacity(
+            allocator,
+            opt.prealloc_objects_num,
+        ),
+    };
+}
+
+pub fn deinit(self: Self) void {
+    for (self.objects.items) |o| {
+        for (o.shapes) |s| {
+            cp.spaceRemoveShape(self.space, s);
+            cp.shapeFree(s);
+        }
+        if (o.body) |bd| {
+            cp.spaceRemoveBody(self.space, bd);
+            cp.bodyFree(bd);
+        }
+        self.allocator.free(o.shapes);
+    }
+    self.objects.deinit();
+    cp.spaceFree(self.space);
+}
+
+/// add object to world
+pub const ObjectOption = struct {
+    pub const BodyProperty = union(enum) {
+        dynamic: struct {
+            position: cp.Vect,
+            velocity: cp.Vect = cp.vzero,
+            angular_velocity: f32 = 0,
+        },
+        kinematic: struct {
+            position: cp.Vect,
+            velocity: cp.Vect = cp.vzero,
+            angular_velocity: f32 = 0,
+        },
+        static: ?struct {
+            position: cp.Vect,
+        },
+    };
+    pub const ShapeProperty = union(enum) {
+        pub const Weight = union(enum) {
+            mass: f32,
+            density: f32,
+        };
+        pub const Physics = struct {
+            weight: Weight = .{ .mass = 0 },
+            elasticity: f32 = 0,
+            friction: f32 = 0.7,
+            is_sensor: bool = false,
+        };
+
+        segment: struct {
+            a: cp.Vect,
+            b: cp.Vect,
+            radius: f32 = 0,
+            physics: Physics = .{},
+        },
+        box: struct {
+            left: f32,
+            right: f32,
+            top: f32,
+            bottom: f32,
+            radius: f32 = 0,
+            physics: Physics = .{},
+        },
+        circle: struct {
+            radius: f32,
+            offset: cp.Vect = cp.vzero,
+            physics: Physics = .{},
+        },
+        polygon: struct {
+            points: []cp.Vert,
+            transform: cp.Transform = cp.transformIdentity,
+            radius: f32 = 0,
+            physics: Physics = .{},
+        },
+    };
+
+    body: BodyProperty,
+    shapes: ShapeProperty,
+    filter: Filter = .{},
+};
+pub fn addObject(self: Self, opt: ObjectOption) !u32 {
+    assert(opt.shapes.len > 0);
+
+    // create physics body
+    var use_global_static = false;
+    var body: *cp.Body = switch (opt.body) {
+        .dynamic => |prop| blk: {
+            var bd: *cp.Body = cp.bodyNew(0, 0).?;
+            cp.bodySetPosition(bd, prop.position);
+            cp.bodySetVelocity(bd, prop.velocity);
+            cp.bodySetAngularVelocity(bd, prop.angular_velocity);
+            break :blk bd.?;
+        },
+        .kinematic => |prop| blk: {
+            var bd: *cp.Body = cp.bodyNewKinematic(0, 0).?;
+            cp.bodySetPosition(bd, prop.position);
+            cp.bodySetVelocity(bd, prop.velocity);
+            cp.bodySetAngularVelocity(bd, prop.angular_velocity);
+            break :blk bd.?;
+        },
+        .static => |prop| blk: {
+            var bd: *cp.Body = undefined;
+            if (prop == null) {
+                bd = cp.spaceGetStaticBody(self.space);
+                use_global_static = true;
+            } else {
+                bd = cp.bodyNewStatic(self.space).?;
+                cp.bodySetPosition(bd, prop.position);
+            }
+            break :blk bd.?;
+        },
+    };
+    if (!use_global_static) _ = cp.spaceAddBody(self.space, body);
+    errdefer {
+        cp.spaceRemoveBody(body);
+        cp.bodyFree(body);
+    }
+
+    // create shapes
+    var shapes = try self.allocator.alloc(*cp.Shape, opt.shapes.len);
+    for (opt.shapes) |s, i| {
+        shapes[i] = switch (s) {
+            .segment => |prop| blk: {
+                var shape = cp.segmentShapeNew(body, prop.a, prop.b, prop.radius).?;
+                initPhysicsOfShape(shape, prop.physics);
+                break :blk shape;
+            },
+            .box => |prop| blk: {
+                var shape = cp.boxShapeNew2(body, .{
+                    .l = prop.left,
+                    .b = prop.bottom,
+                    .r = prop.right,
+                    .t = prop.top,
+                }, prop.radius).?;
+                initPhysicsOfShape(shape, prop.physics);
+                break :blk shape;
+            },
+            .circle => |prop| blk: {
+                var shape = cp.circleShapeNew(body, prop.radius, prop.offset);
+                initPhysicsOfShape(shape, prop.physics);
+                break :blk shape;
+            },
+            .polygon => |prop| blk: {
+                var shape = cp.polyShapeNew(
+                    body,
+                    @intCast(c_int, prop.points.len),
+                    prop.points.ptr,
+                    prop.transform,
+                    prop.radius,
+                );
+                initPhysicsOfShape(shape, prop.physics);
+                break :blk shape;
+            },
+        };
+        cp.shapeSetFilter(shapes[i], .{
+            .group = @intCast(usize, opt.filter.group),
+            .categories = @intCast(c_uint, opt.filter.categories),
+            .mask = @intCast(c_uint, opt.filter.mask),
+        });
+    }
+    errdefer {
+        for (shapes) |s| {
+            cp.spaceRemoveShape(self.space, s);
+            cp.shapeFree(s);
+        }
+        self.allocator.free(shapes);
+    }
+
+    try self.objects.append(.{
+        .body = if (use_global_static) null else body,
+        .shapes = shapes,
+        .filter = opt.filter,
+    });
+    return self.objects.items.len - 1;
+}
+
+fn initPhysicsOfShape(shape: *cp.Shape, phy: ObjectOption.ShapeProperty.Physics) void {
+    switch (phy.weight) {
+        .mass => |m| cp.shapeSetMass(shape, m),
+        .density => |d| cp.shapeSetDensity(shape, d),
+    }
+    cp.shapeSetElasticity(shape, phy.elasticity);
+    cp.shapeSetFriction(shape, phy.friction);
+    cp.shapeSetSensor(shape, @as(u8, @boolToInt(phy.is_sensor)));
+}
+
+/// update world
+pub fn update(self: Self, delta_tick: f32) void {
+    cp.spaceStep(self.space, delta_tick);
+}
+
+/// debug draw
+pub fn debugDraw(self: Self) void {
+    _ = self;
+}
